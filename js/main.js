d44268105946bf9f5d77248d5928e0dafdc3484f
Carlos Sánchez <carlosasanchez1976@gmail.com>
2023-10-18 10:33:02 -0300

    CReación de objeto JS y vinculación código en index.html

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@
 // Comisión 23519
 // Grupo 22
 
+let Parque = {
+    nombre: "Rivadavia",
+    barrio: "Caballito",
+    descripcion: "Área verde con un monumento a Simón Bolívar, librerías de segunda mano y venta de vinilos vintage cada semana.",
+    coordenadasGEO: "9HJ8+VP Buenos Aires",
+    juegosinfantiles: true,
+    senderismo: true,
+    feria: true
+}
